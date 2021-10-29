eddeaff0cdb01ce78f48c28f9b58fb21d8cc5e6f
blame110 <pmpinformatica@gmail.com>
2021-10-29 14:03:35 +0200

    Actualizacion 29 oct

---
+++ src/hilos/VerUsoHardware.java
@@ -0,0 +1,16 @@
+package hilos;
+
+public class VerUsoHardware {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		DatosHardware dh = new DatosHardware();
+
+		System.out.println(dh.Info());
+
+		dh.printUsage();
+
+	}
+
+}
